fbb61ac93d8e871dc97523c849b13e28ad8d6f06
Videxter <andrez.house@gmail.com>
2018-09-16 15:42:49 -0500

    Update thelastpanels.js

--- thelastpanels.js
+++ thelastpanels.js
@@ -1,19 +1,32 @@
 var pain = false, ang = 0;
 
 function setup() {
-  createCanvas(windowWidth, 300);
+  createCanvas(windowWidth, 600);
 	angleMode(DEGREES);
   frameRate(30);
 }
 
 function draw() {
-  background(2);
+  background(200,1);
 	translate(width/2, height/2);
-	ang = spiral(100,50, ang, 60);
-	panels(40,width,pain,45);
-	
-	//pain = !pain;
+	//ang = spiral(100,50, ang, 74);
+	//panels(40,width,pain,45);
+	lissajous(2);
+	//if (frameCount%10 == 0 || frameCount%11 == 0) pain = !pain
 }
+
+function lissajous (natural = 1,a = 120){
+	push();
+	scale(1);
+	stroke(2);
+	strokeWeight(3);
+	var x = frameCount;
+	//point(a*cos(millis()),a*2*sin(natural*millis()));
+	line(a*cos(x),a*sin(x),a*cos(x+1),a*sin(x+1));
+	//line(a*cos(millis()),a*2*sin(natural*millis()),a*cos(millis()+1/millis()),a*2*sin(natural*millis()+1/millis()));
+	pop();
+} 
+
 function panels (weight = 80, widht = windowWidth , paint = true, angle=45 , rectan = 1) {
 	push();
 	rotate(angle);
@@ -22,7 +35,9 @@ function panels (weight = 80, widht = windowWidth , paint = true, angle=45 , rec
 	rectMode(CENTER);
 	
 	for(let i = weight; i <= width; i+= 2*weight){
-		if (paint == true) stroke(0);
+		if (paint == true){ 
+			stroke(0);
+		}
 		else noStroke();
 		//stroke(i*0.2);
 		rect(0,0,i*rectan,i);	
